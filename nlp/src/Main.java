adbce528d028f0159216d1bfeae7bdce394fc053
Avunz <felix@zuluboom.net>
2023-11-29 19:04:41 -0500

    initial commit 2

--- nlp/src/Main.java
+++ nlp/src/Main.java
@@ -1,5 +1,7 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
